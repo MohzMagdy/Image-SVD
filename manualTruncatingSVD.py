11415900b491f76efca6dc974d487705ad10b984
HeshamElsaman <hesham_ahmed_@outlook.com>
2022-02-02 17:32:46 +0200

    Created a manual truncating algorithm

---
+++ manualTruncatingSVD.py
@@ -0,0 +1,20 @@
+from reducedSVD import *
+
+
+def mtsvd(M):
+    A = array(M)
+    siz = shape(A)
+    U, S, V = svd(A)
+    Sval_ = list(diag(S))
+    n = 0
+    Alsum = sum(Sval_)
+    r = len(Sval_)
+    for i in range(len(Sval_)):
+        n += Sval_[i]
+        if n/Alsum > 0.9:
+            r = i
+            break
+    Vr = V[:, :r].copy()
+    Sr = diag(Sval_[:r].copy())
+    Ur = U[:, :r].copy()
+    return Ur, Sr, Vr
